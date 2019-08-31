d55f30778123e8606774a229b5ad55c77f70de8f
Manuel Kienlein <manuelk2000@ulinky.de>
2019-08-31 23:33:41 +0200

    Base structure added and splitted files

---
+++ backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+case "$1" in
+	backup)
+		echo "This function is still in development!"
+	;;
+	restore)
+		echo "This function is still in development!"
+	;;
+	help)
+		echo "This function is still in development!"
+	;;
